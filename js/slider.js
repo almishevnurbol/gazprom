8c8201c651469f7d2ce9857bcbdc3e8902c8d520
Almishev Nurbol <almishev.01@gmail.com>
2021-12-13 23:43:06 +0600

    fix

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.progress-slider', {
+    const swiperProgress = new Swiper('.progress-slider', {
         navigation: {
           nextEl: '.progress-button-next',
           prevEl: '.progress-button-prev',
@@ -7,4 +7,26 @@ document.addEventListener('DOMContentLoaded', () => {
         slidesPerView: 4,
         spaceBetween: 40,
     })
+
+    const swiperSpecprojects = new Swiper('.specprojects-slider', {
+      navigation: {
+        nextEl: '.specprojects-button-next',
+        prevEl: '.specprojects-button-prev',
+      },
+      pagination: {
+        el: '.specprojects-pagination',
+        type: 'bullets',
+      },
+      slidesPerView: 3,
+      spaceBetween: 40,
+  })
+
+  const swiperNext = new Swiper('.next-slider', {
+    navigation: {
+      nextEl: '.next-button-next',
+      prevEl: '.next-button-prev',
+    },
+    slidesPerView: 3,
+    spaceBetween: 40,
+})
 })
